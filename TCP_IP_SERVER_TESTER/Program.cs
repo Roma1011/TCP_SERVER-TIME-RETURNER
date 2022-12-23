f4cf1fe0f6e64d55eab44c1a8ef3415418752b3b
R.Kekua <rkekua@seu.edu.ge>
2022-12-23 11:01:23 +0400

    Create Client socket list for many connection clients also initialize byte array and byte array size for my case and port you can change,last one is port connection server and client

--- TCP_IP_SERVER_TESTER/Program.cs
+++ TCP_IP_SERVER_TESTER/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
 
 namespace TCP_IP_SERVER_TESTER
 {
 	class Program
 	{
+		//initialize server socket type 
+		private static readonly Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		// Client Socket List
+		private static readonly List<Socket> _clientsocket = new List<Socket>();
+		//recived buffer size
+		private const int BUFFER_SIZE = 2048;
+		// connection port server and client
+		private const int PORT = 5555;
+		// recived byte array 
+		private static readonly byte[] _buffer = new byte[BUFFER_SIZE];
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
 		}
 	}
 }
